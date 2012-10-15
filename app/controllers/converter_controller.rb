19fd39442120b40cd9324c2baaaa5b2ae146163a
Tamas Drahos <drahostamas@gmail.com>
2012-10-15 21:06:30 +0200

    controller

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,2 +1,13 @@
 class ConverterController < ApplicationController
+
+  def index
+  end
+
+  def phrase
+    if params[:number]
+      @number = params[:number].to_i
+      @english_phrase = EnglishPhrase.new( @number ).to_s
+    end
+  end
+
 end
